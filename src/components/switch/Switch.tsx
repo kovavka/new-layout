881374005bcf455070a740ef8cdd2ce0913fa07c
Galina Kapger <gikapger@gmail.com>
2020-01-31 01:57:46 +0500

    settings page and general styles

---
+++ src/components/switch/Switch.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import './switch.less'
+
+type SwitchProps = {
+    onToggle: () => void,
+    switched: boolean
+}
+
+export class Switch extends React.Component<SwitchProps> {
+    constructor(props: SwitchProps) {
+        super(props)
+    }
+
+    onToggle() {
+        this.props.onToggle()
+    }
+
+    render() {
+        return (
+            <div className='switch' onClick={() => this.onToggle()}>
+                <div className={'switch__box' + (this.props.switched ? ' switch__box--on' : '')}></div>
+                <div className={'switch__button' + (this.props.switched ? ' switch__button--on' : '')}></div>
+            </div>
+        )
+    }
+}
--- src/screens/Settings/SettingsScreen.tsx
+++ src/screens/Settings/SettingsScreen.tsx
@@ -1,15 +1,71 @@
 import * as React from "react";
 import {StateService} from '../../services/StateService'
 import { TopPanel } from "../../components/top-panel/TopPanel";
+import { Switch } from "../../components/switch/Switch";
 
-export class SettingsScreen extends React.Component {
+type IState = {
+    switched: boolean
+}
+
+export class SettingsScreen extends React.Component<{}, IState> {
+
+
     stateService: StateService = StateService.instance
+
+    state = {switched: false}
+
+    private onSingleDeviceModeToggle() {
+        this.setState({
+            switched: !this.state.switched
+        })
+    }
 
     render() {
         return (
-            <div className="">
+            <div className="page-setting">
                 <TopPanel />
-                <div>Super long long long name</div>
+                <div className="page-setting__name">Super long long long name</div>
+                <div className="page-setting__section">
+                    <div className="page-setting__section-title">Language</div>
+                    <div className="page-setting__section-content">
+                        <div className="btn btn--rounded btn--small btn--active">En</div>
+                        <div className="btn btn--rounded btn--small">Ru</div>
+                    </div>
+                </div>
+                <div className="page-setting__section">
+                    <div className="page-setting__section-title">Theme</div>
+                    <div className="page-setting__section-content">
+                        <div className="theme theme--selected">
+                            <div className="theme__visual">
+                                <div className="theme__visual-inner">
+                                    <div className="theme__secondary-color" style={{backgroundColor: "#B8C0D1"}}></div>
+                                    <div className="theme__text-color" style={{backgroundColor: "#000000"}}></div>
+                                    <div className="theme__primary-color" style={{backgroundColor: "#1565C0"}}></div>
+                                </div>
+                            </div>
+                            <div className="theme__name">Day</div>
+                        </div>
+                        <div className="theme">
+                            <div className="theme__visual">
+                                <div className="theme__visual-inner">
+                                    <div className="theme__secondary-color" style={{backgroundColor: "#B8C0D1"}}></div>
+                                    <div className="theme__text-color" style={{backgroundColor: "#000000"}}></div>
+                                    <div className="theme__primary-color" style={{backgroundColor: "#1565C0"}}></div>
+                                </div>
+                            </div>
+                            <div className="theme__name">Night</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="page-setting__section">
+                    <div className="switch-setting">
+                        <Switch switched={this.state.switched} onToggle={this.onSingleDeviceModeToggle.bind(this)} />
+                        <div className="switch-setting__description">
+                            <div className="switch-setting__caption">Single device mode</div>
+                            <div className="switch-setting__info">Turn on if you use one device on table during the game</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
